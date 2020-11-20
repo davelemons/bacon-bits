5ae1540d4cf4a2843ebf09b75899dce2a348dbe3
Dave Lemons <dave@davelemons.com>
2020-11-20 15:30:59 -0500

    making changes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Dave Was Here!
+          Dave Was Here2!
         </p>
         <a
           className="App-link"
