a55828ab00bb8dda8988db1e5230d275f7ec3ffb
Dave Lemons <dave@davelemons.com>
2020-11-23 10:51:45 -0500

    basic containers

--- src/App.js
+++ src/App.js
@@ -1,18 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { AmplifyAuthenticator, AmplifySignOut, AmplifySignIn } from '@aws-amplify/ui-react';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import {AppBar, Toolbar, Grid, Paper, Typography, Button, IconButton, Container} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  container: {
+    paddingTop: '10px',
+  }
+}));
 
 function App() {
-  return (
-    <div className="App">
-      <header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!</h1>
-      </header>
-      <AmplifySignOut />
+  const classes = useStyles();
+  const [authState, setAuthState] = React.useState();
+  const [user, setUser] = React.useState();
+
+  React.useEffect(() => {
+      return onAuthUIStateChange((nextAuthState, authData) => {
+          setAuthState(nextAuthState);
+          setUser(authData)
+      });
+  }, []);
+
+return authState === AuthState.SignedIn && user ? (
+    <div className={classes.root}>
+
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Bacon Bits
+          </Typography>
+          <Button color="inherit">Logout</Button>
+        </Toolbar>
+      </AppBar>
+      {/* <div>Hello, {user.username}</div> */}
+      <Container className={classes.container} maxWidth="xl">
+        <Grid container spacing={3}>
+          <Grid item xs>
+            <Paper className={classes.paper}>xs</Paper>
+          </Grid>
+          <Grid item xs>
+            <Paper className={classes.paper}>xs</Paper>
+          </Grid>
+          <Grid item xs>
+            <Paper className={classes.paper}>xs</Paper>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
-  );
+  ) : (
+    <AmplifyAuthenticator>
+      <AmplifySignIn slot="sign-in" hideSignUp></AmplifySignIn>
+    </AmplifyAuthenticator>
+);
 }
 
-export default withAuthenticator(App);
+export default App;
